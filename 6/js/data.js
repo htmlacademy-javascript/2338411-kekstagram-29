606fdf8505898aed5e237ec7a99539db451c7322
Keks <keks@htmlacademy.ru>
2023-07-05 16:31:55 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/data.js
@@ -0,0 +1,74 @@
+import {getRandomArrayElement, getRandomInteger} from './util.js';
+
+const PHOTO_COUNT = 25;
+
+const MAX_COMMENT_COUNT = 30;
+
+//массив описания фото
+const DESCRIPTIONPHOTO = [
+  'Лето',
+  'Вкуснятина',
+  'Огонь',
+  'Carpe Diem!',
+  'Моменты'
+];
+//массив сообщений
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+//массив имен
+const NAMES = [
+  'Евгений',
+  'Дмитрий',
+  'Александр',
+  'Леонард',
+  'Елисей',
+  'Геннадий',
+  'Вадим',
+  'Борис',
+  'Анна',
+  'Жанна',
+  'Лидия',
+  'Галина',
+  'Даниил',
+  'Влад',
+  'Олег',
+  'Мария',
+  'Слава',
+  'Майя',
+  'Людмила',
+  'Любовь',
+  'Татьяна',
+  'Антон',
+  'Михаил',
+  'Анастасия',
+  'Денис',
+  'Клавдия'
+];
+
+//создаем комментарий
+const createComment = () => ({
+  id: getRandomInteger(1, 26),
+  avatar: `img/avatar-${getRandomInteger(1,6)}.svg`,
+  message: getRandomArrayElement(MESSAGES),
+  name: getRandomArrayElement(NAMES)
+});
+//массив комментариев
+const createComments = (count) => Array.from ({length: count}, createComment);
+
+//массив фотографий
+const createPhoto = () => ({
+  id: getRandomInteger(1, 25),
+  url: `photos/${getRandomInteger(1, 25)}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONPHOTO),
+  likes: getRandomInteger(15,200),
+  comments: createComments(getRandomInteger(0, MAX_COMMENT_COUNT))
+});
+const createPhotos = () => Array.from({length:PHOTO_COUNT}, createPhoto);
+
+export {createPhotos};
---
+++ 6/js/function.js
@@ -0,0 +1,50 @@
+/**
+ * проверяет строку на количество символов
+ * @param {string} string
+ * @param {int} length
+ * @returns {boolean}
+ */
+const checkStringLength = (string, length = 10) => string.length <= length;
+checkStringLength('наконец-то я сделала это', 5);
+checkStringLength('наконец-то я сделала это');
+/**
+ * Проверка строки на палиндром
+ * @param {string} rawString - строка для проверки
+ * @returns {Boolean} истинна если палиндром, ложь если нет
+ */
+function isPalindrom(rawString) {
+  const string = rawString.replaceAll(' ', '').toLowerCase();
+  for (let i = 0; i < string.length / 2; i++) {
+    if (string[i] !== string[string.length - i - 1]) {
+      return false;
+    }
+    return true;
+  }
+}
+console.log(isPalindrom('топот'));
+console.log(isPalindrom('довОд'));
+console.log(isPalindrom('Кекс'));
+console.log(isPalindrom('Лёша на полке клопа нашёл'));
+
+//Время указывается в виде строки в формате часы:минуты
+const getTime = (stringTime) => {
+  const [hours, minutes] = stringTime.split(':').map(Number);
+  return hours * 60 + minutes;
+};
+
+//функция, которая принимает время начала и конца рабочего дня, а также время старта и продолжительность встречи в минутах и возвращает true, если встреча не выходит за рамки рабочего дня, и false, если выходит.
+const isWorkHours = (startTime, endTime, meetingStart, meetingContinueInMinutes) => {
+  const meetingEnd = getTime(meetingStart) + meetingContinueInMinutes;
+  const overTime = getTime(endTime);
+
+  if (meetingEnd <= overTime && getTime(meetingStart) >= getTime(startTime)) {
+    return true;
+  }
+  return false;
+};
+
+isWorkHours('08:00', '17:30', '14:00', 90);
+isWorkHours('8:0', '10:0', '8:0', 120);
+isWorkHours('08:00', '14:30', '14:00', 90);
+isWorkHours('14:00', '17:30', '08:0', 90);
+isWorkHours('8:00', '17:30', '08:00', 900);
