3ebb363daef9025f0d702a13e8bf15f80b4c7456
Keks <keks@htmlacademy.ru>
2023-07-05 09:44:24 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/util.js
@@ -0,0 +1,11 @@
+//функция по получению случайпного числа из диапазона
+const getRandomInteger = (a,b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+//разделяй и властвуй
+const getRandomArrayElement = (elements) => elements [getRandomInteger(0, elements.length - 1)];
+
+export {getRandomArrayElement, getRandomInteger};
